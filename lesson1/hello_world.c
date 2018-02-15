6f81401599d79ae8eebd05b96b7c0cdf93224fdb
Sam McSweeney <sammy.mcsweeney@gmail.com>
2018-02-15 10:04:12 +0800

    Init commit. Added a hello world program to lesson 1.

---
+++ lesson1/hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // <-- This is where the printf() family of functions lives
+
+int main( int argc, char *argv[] )
+{
+    // Print out the canonical message to stdout
+    printf( "Hello world!\n" );
+
+    // Exit successfully
+    return 0;
+}
