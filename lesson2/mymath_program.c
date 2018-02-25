2f25a35cff02ac18ea10fdb185112994d9816fad
Sam McSweeney <sammy.mcsweeney@gmail.com>
2018-02-25 08:24:38 +0800

    Still working on lesson #2

---
+++ lesson2/mymath_program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+/* A demonstration of the functions written in mymath.c" */
+{
+    // Make some numbers
+    double x = 5.0, y = 4.0;
+
+    // Do some calculations
+    double a = add(x,y);
+    double s = subtract(x,y);
+    double m = multiply(x,y);
+    double d = divide(x,y);
+
+    // Print out the results
+    printf( "x + y = %.1lf\n", a );
+    printf( "x - y = %.1lf\n", s );
+    printf( "x * y = %.1lf\n", m );
+    printf( "x / y = %.2lf\n", d );
+}
