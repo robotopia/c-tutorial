f5bc813e658c4ba6b6bd53419be0505a553bd240
Sam McSweeney <sammy.mcsweeney@gmail.com>
2018-02-26 14:47:47 +0800

    Started work on Act 4 of lesson 2

--- lesson2/struct.c
+++ lesson2/struct.c
@@ -8,8 +8,8 @@
  * a programming language already (usually Python), and who want to understand
  * the C language and syntax a bit better.
  *
- * This program produces a segfault, by design, by addressing memory addresses
- * that haven't been allocated, or which are disallowed.
+ * This program demostrates the basic use of structs. It implements a struct
+ * for representing 3D points
  *
  *****************************************************************************/
 
@@ -17,14 +17,4 @@
 
 int main()
 {
-    // Let's make a pointer and initialise it to NULL (=0)
-    int *ptr = NULL;
-
-    // And now let's try and see what's at that memory address.
-    // Memory address 0 is forbidden!
-    int i = *ptr;
-    printf( "The value %d is at memory address %p\n", i, ptr );
-
-    // Return success!
-    return 0;
 }
